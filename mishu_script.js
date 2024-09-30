4709aa7e63d85f1083acdd353a55fd31bc87032b
Macarieeee <116794024+Macarieeee@users.noreply.github.com>
2024-09-30 20:52:25 +0300

    Add files via upload

--- mishu_script.js
+++ mishu_script.js
@@ -1,14 +1,29 @@
+function filterCategory(category) {
+  var items = document.getElementsByClassName('menu-items');
+  
+  for (var i = 0; i < items.length; i++) {
+      if (category === 'all') {
+          items[i].style.display = 'flex'; // Show all items
+      } else {
+          if (items[i].classList.contains(category)) {
+              items[i].style.display = 'flex'; // Show matching category
+          } else {
+              items[i].style.display = 'none'; // Hide other categories
+          }
+      }
+  }
+}
 const buttonRight = document.getElementById('slideRight');
-    const buttonLeft = document.getElementById('slideLeft');
+  const buttonLeft = document.getElementById('slideLeft');
 
-    buttonRight.onclick = function () {
-      document.getElementById('container').scrollLeft += 120;
-    };
-    buttonLeft.onclick = function () {
-      document.getElementById('container').scrollLeft -= 120;
-    };
+  buttonRight.onclick = function () {
+    document.getElementById('categories').scrollLeft += 120;
+  };
+  buttonLeft.onclick = function () {
+    document.getElementById('categories').scrollLeft -= 120;
+  };
 
-    document.querySelector('.preparate-porc').addEventListener('click', () => {
-      document.querySelector('.menu-category-05').classList.add('myClass');
-      document.querySelector('.menu-all').classList.add('myClass2');
-    });
+    function scrollToNextSection() {
+      var nextSection = document.querySelector("#menu-section");
+      nextSection.scrollIntoView({ behavior: "smooth" });
+  }
